03a6416fa416094cf6566f7d98a901ae0c7b2d70
Kohei Komuro <82309237+k-sml@users.noreply.github.com>
2023-05-28 23:06:07 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	// "log"
+	// "gostudy/application/config"
+	"gostudy/application/app/controllers"
+	"gostudy/application/app/models"
+)
+
+func main() {
+	fmt.Println(models.Db)
+
+	controllers.StartMainServer()
+
+}
